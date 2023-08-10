9e8176a7f726bd231169cb77b8fb36223f35a9bf
BinaryBird <45174146+Khimer@users.noreply.github.com>
2023-08-10 17:14:10 +0700

    Update install.sh

--- script/install.sh
+++ script/install.sh
@@ -3,6 +3,7 @@ cd sam; pip install -e .
 cd -
 
 # Install Grounding-Dino
+pip install -e git+https://github.com/IDEA-Research/GroundingDINO.git@main#egg=GroundingDINO
 pip install git+https://github.com/IDEA-Research/GroundingDINO.git@main#egg=GroundingDINO
 
 # Install other lib
