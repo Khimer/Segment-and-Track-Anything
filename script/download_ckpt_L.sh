cb939f465eb0083cc66a99bca17cf0ed1149fe40
BinaryBird <45174146+Khimer@users.noreply.github.com>
2023-10-06 22:48:12 +0700

    Update download_ckpt_L.sh

--- script/download_ckpt_L.sh
+++ script/download_ckpt_L.sh
@@ -1,3 +1,3 @@
-wget https://dl.fbaipublicfiles.com/segment_anything/sam_vit_l_0b3195.pth && mv sam_vit_l_0b3195.pth ./ckpt
+wget https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth && mv sam_vit_h_4b8939.pth ./ckpt
 gdown --id '1Osyoz9u53CdFw8cqakiVQcFKCh0qfr8f' --output ./ckpt/R50_DeAOTL_PRE_YTB_DAV.pth
 gdown --id '1XRkjlPnj3BVtCOLKJVayvTDAN4VbbhZS' --output ./ckpt/groundingdino_swint_ogc.pth
